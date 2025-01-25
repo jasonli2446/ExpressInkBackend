4db75479c2a697e9c0f2aa2725bb63b793812e1b
Sunveer Chugh <sunveerchugh@gmail.com>
2025-01-24 20:41:11 -0500

    packages and set up express app

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-//test
+const express = require('express');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+
+//To do: make an env file for the openai api key (or omnistack??)
+require('dotenv').config();
+
+
+//set app running on port 3000
+const app = express();
+const port = process.env.PORT || 3000;
